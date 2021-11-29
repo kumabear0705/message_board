03962c9aa58aff55165ab0d468f3fbd9878f7e9f
yuya.kumakura <yuyabear1999@gmail.com>
2021-11-29 14:32:36 +0900

    データベース関連のファイルを追加

---
+++ src/models/Message.java
@@ -0,0 +1,73 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.google.protobuf.Timestamp;
+
+@Entity
+@Table
+public class Message {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "content", length = 255, nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createf_at;
+
+    @Column(name = "update_at", nullable = false)
+    private Timestamp update_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getCreatef_at() {
+        return createf_at;
+    }
+
+    public void setCreatef_at(Timestamp createf_at) {
+        this.createf_at = createf_at;
+    }
+
+    public Timestamp getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Timestamp update_at) {
+        this.update_at = update_at;
+    }
+
+
+}
